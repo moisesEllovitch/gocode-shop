886f44df2ddfefb630b61d0d8472f28f43791391
Moises David Ellovitch <moisesello@hotmail.com>
2021-02-03 00:52:53 +0200

    Add products mock with structural directive ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PRODUCTS } from './mock-products';
+import { Product } from './product';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gocode-shop';
+  products : Product[] = PRODUCTS;
 }
